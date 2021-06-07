5d4e11524dcdcea5b3046e116da350467d2af850
976Evil <an.lebedev@mail.ru>
2021-06-07 19:09:31 +0300

    media add

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from pruducts.views import index,products
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('products/', products, name='products'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
